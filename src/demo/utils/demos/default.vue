113178f4256f9946ffc368ab89bc3b8521cff18c
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-21 19:37:41 -0500

    chore(demo): add global styles, variables, and mixins for TvArticle component

---
+++ src/demo/utils/demos/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <TvArticle />
+</template>
+
+<script setup>
+import { TvMenu } from '@todovue/tv-menu';
+
+</script>
---
+++ src/demo/utils/mocks.js
@@ -0,0 +1,10 @@
+import Default from './demos/default.vue?raw';
+
+export const demos = [
+  {
+    id: 1,
+    title: "TvArticle",
+    propsData: {},
+    html: Default,
+  },
+];
